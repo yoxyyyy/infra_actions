08ff10ff513f7ba323ba92848e11e8a2f1e8d75a
Александров Илья <ybalnagoi@gmail.com>
2023-05-15 19:37:16 +0300

    q

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -20,5 +20,5 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get('/second/')
         self.assertContains(response, 'А это вторая страница!')
